67c302553e57272c3e4d6bb14384b2d7c7a2b7fc
InsaneChronos <121401167+InsaneChronos@users.noreply.github.com>
2023-09-26 13:21:20 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "200px";
+  document.getElementById("close-menu").style.transform = "rotate(90deg)";
+  document.getElementById("open-menu").style.display = "none";
+  document.getElementById("close-menu").style.display = "flex";
+  document.getElementById("social").style.marginLeft = "200px";
+}
+
+function closeNav() {
+	document.getElementById("mySidenav").style.width = "0";
+	document.getElementById("open-menu").style.transform = "rotate(0deg)";
+	document.getElementById("close-menu").style.display = "none";
+	document.getElementById("open-menu").style.display = "flex";
+	document.getElementById("social").style.marginLeft = "0px";
+	}
