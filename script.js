4c6f8feed5fdbf1cb778d60fe94d39781e26256f
InsaneChronos <121401167+InsaneChronos@users.noreply.github.com>
2023-09-27 13:47:30 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,53 @@
+document.querySelectorAll('a[href="'+document.URL+'"]').forEach(link => link.className += 'current-link');
+
 function openNav() {
-  document.getElementById("mySidenav").style.width = "220px";
+  document.getElementById("sidenav").style.width = "220px";
   document.getElementById("close-menu").style.transform = "rotate(90deg)";
-  document.getElementById("open-menu").style.display = "none";
   document.getElementById("close-menu").style.display = "flex";
   document.getElementById("social").style.marginLeft = "220px";
 }
 
 function closeNav() {
-	document.getElementById("mySidenav").style.width = "0";
+	document.getElementById("sidenav").style.width = "0";
 	document.getElementById("open-menu").style.transform = "rotate(0deg)";
-	document.getElementById("close-menu").style.display = "none";
 	document.getElementById("open-menu").style.display = "flex";
 	document.getElementById("social").style.marginLeft = "0px";
+	}
+
+function selectLangRu() {
+	
+	var langOpacity = document.getElementById("langContainer");
+	var actualOpacity = getComputedStyle(langOpacity).opacity;
+	
+	document.getElementById("langContainer").style.opacity = "1";
+	document.getElementById("ruLangEn").style.display = "flex";
+	document.getElementById("langContainer").style.marginTop = "40px";
+	
+	if (actualOpacity == "1") {
+		langOpacity.style.opacity = "0";
+		langOpacity.style.marginTop = "-40px";
+		}
+	
+	}
+
+function selectLangEn() {
+	
+	var langOpacity = document.getElementById("langContainer");
+	var actualOpacity = getComputedStyle(langOpacity).opacity;
+	
+	document.getElementById("langContainer").style.opacity = "1";
+	document.getElementById("enLangRu").style.display = "flex";
+	document.getElementById("langContainer").style.marginTop = "40px";
+	
+	if (actualOpacity == "1") {
+		langOpacity.style.opacity = "0";
+		langOpacity.style.marginTop = "-40px";
+		}
+	
+	}
+
+if(window.innerHeight > window.innerWidth){
+	function openNav() {
+		document.getElementById("sidenav").style.width = "100%";
+		}
 	}
